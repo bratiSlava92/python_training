e621e4678b4619fb9511b9d35842633bf24bfabd
bratiSlava92 <2405bratiSlava@gmail.com>
2021-06-16 01:43:06 +0300

    Выделен класс-помощник для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="Ivan", middlename="Petrovich", lastname="Sidorov", nickname="nickname",
+    app.contact.create_new(Contact(firstname="Ivan", middlename="Petrovich", lastname="Sidorov", nickname="nickname",
                                    title="ceo", company="sber", address="address", home_phone="8125556677",
                                    mobile_phone="89998887766", work_phone="8123334455", fax="8123344455", email="test@mail.ru",
                                    email2="test2@mail.ru", email3="test3@mail.ru", homepage="www.test.ru",
