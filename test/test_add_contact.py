f8fabd10553ed2a574d9be4f0087328d57fcfb67
bratiSlava92 <2405bratiSlava@gmail.com>
2021-06-16 01:04:06 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_contact(Contact(firstname="Ivan", middlename="Petrovich", lastname="Sidorov", nickname="nickname",
                                    title="ceo", company="sber", address="address", home_phone="8125556677",
                                    mobile_phone="89998887766", work_phone="8123334455", fax="8123344455", email="test@mail.ru",
                                    email2="test2@mail.ru", email3="test3@mail.ru", homepage="www.test.ru",
                                    address2="address2", phone2="home123", notes="comment"))
-    app.logout()
+    app.session.logout()
