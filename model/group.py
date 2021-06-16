a69a6224dd5bcfa0a1eff9845bfcb447f29a49dd
bratiSlava92 <2405bratiSlava@gmail.com>
2021-06-17 00:52:06 +0300

    Создан тест модификации первой группы

--- model/group.py
+++ model/group.py
@@ -2,6 +2,13 @@
 class Group:
 
     def __init__(self, name, header, footer):
+        # for group creation
         self.name = name
         self.header = header
         self.footer = footer
+
+    def __init__(self, new_name, new_header, new_footer):
+        # for modify_first_group
+        self.newName = new_name
+        self.newHeader = new_header
+        self.newFooter = new_footer
