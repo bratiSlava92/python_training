6a2c27be3b9013cdac963a752ac9aa35202cd30e
bratiSlava92 <2405bratiSlava@gmail.com>
2021-06-19 23:22:51 +0300

    Сделана проверка валидности фикстуры

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,5 @@ from model.contact import Contact
 
 
 def test_modify_main_contact_data(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_contact(Contact(firstname="Имя", middlename="Отчество", lastname="Фамилия", address="изменен",
                                        mobile_phone="89998887766", email="test@mail.ru", bday="4", bmonth="April", byear="1990"))
-    app.session.logout()
